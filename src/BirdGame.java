667dfab39690329543000be996e0f3eafcb42f29
chro-Mia <tessa.chromia@gmail.com>
2025-05-05 14:14:20 -0700

    setup

---
+++ src/BirdGame.java
@@ -0,0 +1,12 @@
+import processing.core.PApplet;
+
+public class BirdGame extends PApplet{
+
+    public void settings(){ size(650, 650); }
+
+    public void setup(){}
+
+    public void keyPressed(){}
+
+    public void draw(){}
+}
